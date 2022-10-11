05b70090c30bc111e63275b07759c8053f051520
Gabriel Tarzia <gabrieltarzia@gmail.com>
2022-10-11 16:14:44 -0300

    Added freetogames.com banner

--- lib/app/modules/homeView/home.view.dart
+++ lib/app/modules/homeView/home.view.dart
@@ -2,13 +2,37 @@ import 'package:flutter/material.dart';
 import '../gameList/gameList.widget.dart';
 
 class HomeView extends StatelessWidget {
-  HomeView({Key? key}) : super(key: key);
+  const HomeView({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Colors.blueGrey.shade900,
-      body: GameListWidget(),
+      body: Column(
+        children: [
+          Expanded(
+              flex: 1,
+              child: Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  children: [
+                    const Text(
+                      'Data by: ',
+                      textAlign: TextAlign.center,
+                      style: TextStyle(
+                        color: Colors.white54,
+                      ),
+                    ),
+                    Image.network(
+                        'https://www.freetogame.com/assets/images/freetogame-logo.png'),
+                  ],
+                ),
+              )),
+          Expanded(flex: 16, child: GameListWidget())
+        ],
+      ),
     );
   }
 }
